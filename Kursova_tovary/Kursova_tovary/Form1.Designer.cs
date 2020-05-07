00c91e1d6bceb288990687b74b662bda7b7b6b50
Epozum <56679515+Epozum@users.noreply.github.com>
2020-05-07 18:10:03 +0300

    Add files via upload

---
+++ Kursova_tovary/Kursova_tovary/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Kursova_tovary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDesc = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BTN_INSERT = new System.Windows.Forms.Button();
+            this.BTN_DELETE = new System.Windows.Forms.Button();
+            this.BTN_UPDATE = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.BTN_CLEAT = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxID.Location = new System.Drawing.Point(102, 35);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(151, 26);
+            this.textBoxID.TabIndex = 0;
+            this.textBoxID.Tag = "TextBoxID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(494, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(503, 485);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(16, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(16, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxName.Location = new System.Drawing.Point(102, 77);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(151, 26);
+            this.textBoxName.TabIndex = 3;
+            this.textBoxName.Tag = "TextBoxName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(16, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Description";
+            // 
+            // textBoxDesc
+            // 
+            this.textBoxDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxDesc.Location = new System.Drawing.Point(102, 114);
+            this.textBoxDesc.Multiline = true;
+            this.textBoxDesc.Name = "textBoxDesc";
+            this.textBoxDesc.Size = new System.Drawing.Size(208, 104);
+            this.textBoxDesc.TabIndex = 5;
+            this.textBoxDesc.Tag = "TextBoxDesc";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 265);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(173, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Tag = "BTN_CHOOSE_IMG";
+            this.button1.Text = "<<<>>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTN_INSERT
+            // 
+            this.BTN_INSERT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTN_INSERT.Location = new System.Drawing.Point(333, 78);
+            this.BTN_INSERT.Name = "BTN_INSERT";
+            this.BTN_INSERT.Size = new System.Drawing.Size(128, 36);
+            this.BTN_INSERT.TabIndex = 11;
+            this.BTN_INSERT.Text = "Insert";
+            this.BTN_INSERT.UseVisualStyleBackColor = true;
+            this.BTN_INSERT.Click += new System.EventHandler(this.BTN_INSERT_Click);
+            // 
+            // BTN_DELETE
+            // 
+            this.BTN_DELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTN_DELETE.Location = new System.Drawing.Point(333, 164);
+            this.BTN_DELETE.Name = "BTN_DELETE";
+            this.BTN_DELETE.Size = new System.Drawing.Size(128, 36);
+            this.BTN_DELETE.TabIndex = 12;
+            this.BTN_DELETE.Text = "Delete";
+            this.BTN_DELETE.UseVisualStyleBackColor = true;
+            this.BTN_DELETE.Click += new System.EventHandler(this.BTN_DELETE_Click);
+            // 
+            // BTN_UPDATE
+            // 
+            this.BTN_UPDATE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTN_UPDATE.Location = new System.Drawing.Point(333, 121);
+            this.BTN_UPDATE.Name = "BTN_UPDATE";
+            this.BTN_UPDATE.Size = new System.Drawing.Size(128, 36);
+            this.BTN_UPDATE.TabIndex = 13;
+            this.BTN_UPDATE.Text = "Update";
+            this.BTN_UPDATE.UseVisualStyleBackColor = true;
+            this.BTN_UPDATE.Click += new System.EventHandler(this.BTN_UPDATE_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(491, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Search:";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxSearch.Location = new System.Drawing.Point(556, 30);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(441, 26);
+            this.textBoxSearch.TabIndex = 14;
+            this.textBoxSearch.Tag = "TextBoxID";
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // BTN_CLEAT
+            // 
+            this.BTN_CLEAT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTN_CLEAT.Location = new System.Drawing.Point(333, 35);
+            this.BTN_CLEAT.Name = "BTN_CLEAT";
+            this.BTN_CLEAT.Size = new System.Drawing.Size(128, 36);
+            this.BTN_CLEAT.TabIndex = 16;
+            this.BTN_CLEAT.Text = "Clear";
+            this.BTN_CLEAT.UseVisualStyleBackColor = true;
+            this.BTN_CLEAT.Click += new System.EventHandler(this.BTN_CLEAT_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(16, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 18);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Number";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNumber.Location = new System.Drawing.Point(102, 233);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(151, 26);
+            this.textBoxNumber.TabIndex = 17;
+            this.textBoxNumber.Tag = "TextBoxNumber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1006, 568);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.BTN_CLEAT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.BTN_UPDATE);
+            this.Controls.Add(this.BTN_DELETE);
+            this.Controls.Add(this.BTN_INSERT);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDesc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDesc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BTN_INSERT;
+        private System.Windows.Forms.Button BTN_DELETE;
+        private System.Windows.Forms.Button BTN_UPDATE;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button BTN_CLEAT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNumber;
+    }
+}
+
---
+++ Kursova_tovary/Kursova_tovary/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Tutorial.SqlConn;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Kursova_tovary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+
+            opf.Filter = "Choose image(*.JPG;*.PNG;*.GIF)|*.jpg; *.png; *.gif";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(opf.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillDGV("");
+        }
+
+        public void FillDGV(string valueToSearch)
+        {
+            MySqlCommand command = new MySqlCommand("SELECT * FROM product WHERE CONCAT (ID, NAME, DESCRIPTION, NUMBER) LIKE '%" + valueToSearch + "%'", DBUtils.GetDBConnection());
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.RowTemplate.Height = 60;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = table;
+            DataGridViewImageColumn imgcol = new DataGridViewImageColumn();
+            imgcol = (DataGridViewImageColumn)dataGridView1.Columns[4];
+            imgcol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            Byte[] img = (Byte[])dataGridView1.CurrentRow.Cells[4].Value;
+            MemoryStream ms = new MemoryStream(img);
+            pictureBox1.Image = Image.FromStream(ms);
+            textBoxID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBoxName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBoxDesc.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBoxNumber.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void BTN_INSERT_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] img = ms.ToArray();
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO product(ID, NAME, DESCRIPTION, NUMBER, IMAGE) VALUES (@id, @name, @desc, @numb, @img)", DBUtils.GetDBConnection());
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxID.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBoxName.Text;
+            command.Parameters.Add("@desc", MySqlDbType.VarChar).Value = textBoxDesc.Text;
+            command.Parameters.Add("@numb", MySqlDbType.VarChar).Value = textBoxDesc.Text;
+            command.Parameters.Add("@img", MySqlDbType.Blob).Value = img;
+            ExecMyQuery(command, "Data Inserted");
+        }
+        
+        public void ExecMyQuery(MySqlCommand mcomd, string myMsg)
+        {
+            mcomd.Connection.Open();
+            if(mcomd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show(myMsg) ;
+            }
+            else
+            {
+                MessageBox.Show("Message not Executed");
+            }
+            mcomd.Connection.Close();
+            FillDGV(""); 
+        }
+
+        private void BTN_UPDATE_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] img = ms.ToArray();
+
+            MySqlCommand command = new MySqlCommand("UPDATE product SET NAME=@name ,DESCRIPTION=@desc, NUMBER = @numb, IMAGE=@img WHERE ID = @id", DBUtils.GetDBConnection());
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxID.Text;
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBoxName.Text;
+            command.Parameters.Add("@desc", MySqlDbType.VarChar).Value = textBoxDesc.Text;
+            command.Parameters.Add("@numb", MySqlDbType.VarChar).Value = textBoxNumber.Text;
+            command.Parameters.Add("@img", MySqlDbType.Blob).Value = img;
+            ExecMyQuery(command, "Data Updated");
+        }
+
+        private void BTN_DELETE_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand("DELETE FROM product WHERE ID=@id", DBUtils.GetDBConnection());
+
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxID.Text;
+
+            ExecMyQuery(command, "Data Deleted");
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            FillDGV(textBoxSearch.Text); 
+        }
+
+        private void BTN_CLEAT_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        public void ClearFields()
+        {
+            textBoxID.Text = "";
+            textBoxName.Text = "";
+            textBoxDesc.Text = "";
+            textBoxNumber.Text = "";
+            pictureBox1.Image = null;
+        }
+    }
+}
