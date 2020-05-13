e78017b0bad720588860e6dddf06ee8da9452af8
Epozum <56679515+Epozum@users.noreply.github.com>
2020-05-13 09:33:20 +0300

    Add files via upload
    Курсова конструювання ПЗ

--- Kursova_tovary/Kursova_tovary/Form1.Designer.cs
+++ Kursova_tovary/Kursova_tovary/Form1.Designer.cs
@@ -45,6 +45,17 @@
             this.BTN_CLEAT = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.keyRad = new System.Windows.Forms.RadioButton();
+            this.gameRad = new System.Windows.Forms.RadioButton();
+            this.mouseRad = new System.Windows.Forms.RadioButton();
+            this.allRad = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxPerCent = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxMarkupPrice = new System.Windows.Forms.TextBox();
+            this.PerCentChange = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -61,9 +72,9 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(494, 71);
+            this.dataGridView1.Location = new System.Drawing.Point(634, 45);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(503, 485);
+            this.dataGridView1.Size = new System.Drawing.Size(583, 577);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
             // 
@@ -113,16 +124,16 @@
             this.textBoxDesc.Location = new System.Drawing.Point(102, 114);
             this.textBoxDesc.Multiline = true;
             this.textBoxDesc.Name = "textBoxDesc";
-            this.textBoxDesc.Size = new System.Drawing.Size(208, 104);
+            this.textBoxDesc.Size = new System.Drawing.Size(151, 104);
             this.textBoxDesc.TabIndex = 5;
             this.textBoxDesc.Tag = "TextBoxDesc";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
-            this.pictureBox1.Location = new System.Drawing.Point(19, 265);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 305);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(442, 262);
+            this.pictureBox1.Size = new System.Drawing.Size(613, 288);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -130,7 +141,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(173, 533);
+            this.button1.Location = new System.Drawing.Point(233, 599);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 10;
@@ -142,7 +153,7 @@
             // BTN_INSERT
             // 
             this.BTN_INSERT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.BTN_INSERT.Location = new System.Drawing.Point(333, 78);
+            this.BTN_INSERT.Location = new System.Drawing.Point(497, 78);
             this.BTN_INSERT.Name = "BTN_INSERT";
             this.BTN_INSERT.Size = new System.Drawing.Size(128, 36);
             this.BTN_INSERT.TabIndex = 11;
@@ -153,7 +164,7 @@
             // BTN_DELETE
             // 
             this.BTN_DELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.BTN_DELETE.Location = new System.Drawing.Point(333, 164);
+            this.BTN_DELETE.Location = new System.Drawing.Point(497, 164);
             this.BTN_DELETE.Name = "BTN_DELETE";
             this.BTN_DELETE.Size = new System.Drawing.Size(128, 36);
             this.BTN_DELETE.TabIndex = 12;
@@ -164,7 +175,7 @@
             // BTN_UPDATE
             // 
             this.BTN_UPDATE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.BTN_UPDATE.Location = new System.Drawing.Point(333, 121);
+            this.BTN_UPDATE.Location = new System.Drawing.Point(497, 121);
             this.BTN_UPDATE.Name = "BTN_UPDATE";
             this.BTN_UPDATE.Size = new System.Drawing.Size(128, 36);
             this.BTN_UPDATE.TabIndex = 13;
@@ -176,7 +187,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(491, 34);
+            this.label4.Location = new System.Drawing.Point(631, 18);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 18);
             this.label4.TabIndex = 15;
@@ -185,9 +196,9 @@
             // textBoxSearch
             // 
             this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxSearch.Location = new System.Drawing.Point(556, 30);
+            this.textBoxSearch.Location = new System.Drawing.Point(696, 13);
             this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(441, 26);
+            this.textBoxSearch.Size = new System.Drawing.Size(521, 26);
             this.textBoxSearch.TabIndex = 14;
             this.textBoxSearch.Tag = "TextBoxID";
             this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
@@ -195,7 +206,7 @@
             // BTN_CLEAT
             // 
             this.BTN_CLEAT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.BTN_CLEAT.Location = new System.Drawing.Point(333, 35);
+            this.BTN_CLEAT.Location = new System.Drawing.Point(497, 35);
             this.BTN_CLEAT.Name = "BTN_CLEAT";
             this.BTN_CLEAT.Size = new System.Drawing.Size(128, 36);
             this.BTN_CLEAT.TabIndex = 16;
@@ -222,12 +233,139 @@
             this.textBoxNumber.TabIndex = 17;
             this.textBoxNumber.Tag = "TextBoxNumber";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(16, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 18);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Price";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPrice.Location = new System.Drawing.Point(102, 273);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(151, 26);
+            this.textBoxPrice.TabIndex = 19;
+            this.textBoxPrice.Tag = "TextBoxPrice";
+            this.textBoxPrice.TextChanged += new System.EventHandler(this.textBoxPrice_TextChanged);
+            // 
+            // keyRad
+            // 
+            this.keyRad.AutoSize = true;
+            this.keyRad.Location = new System.Drawing.Point(497, 206);
+            this.keyRad.Name = "keyRad";
+            this.keyRad.Size = new System.Drawing.Size(70, 17);
+            this.keyRad.TabIndex = 21;
+            this.keyRad.TabStop = true;
+            this.keyRad.Text = "Keyboard";
+            this.keyRad.UseVisualStyleBackColor = true;
+            this.keyRad.CheckedChanged += new System.EventHandler(this.keyRad_CheckedChanged);
+            // 
+            // gameRad
+            // 
+            this.gameRad.AutoSize = true;
+            this.gameRad.Location = new System.Drawing.Point(497, 229);
+            this.gameRad.Name = "gameRad";
+            this.gameRad.Size = new System.Drawing.Size(71, 17);
+            this.gameRad.TabIndex = 22;
+            this.gameRad.TabStop = true;
+            this.gameRad.Text = "Gamepad";
+            this.gameRad.UseVisualStyleBackColor = true;
+            this.gameRad.CheckedChanged += new System.EventHandler(this.gameRad_CheckedChanged);
+            // 
+            // mouseRad
+            // 
+            this.mouseRad.AutoSize = true;
+            this.mouseRad.Location = new System.Drawing.Point(497, 252);
+            this.mouseRad.Name = "mouseRad";
+            this.mouseRad.Size = new System.Drawing.Size(57, 17);
+            this.mouseRad.TabIndex = 23;
+            this.mouseRad.TabStop = true;
+            this.mouseRad.Text = "Mouse";
+            this.mouseRad.UseVisualStyleBackColor = true;
+            this.mouseRad.CheckedChanged += new System.EventHandler(this.mouseRad_CheckedChanged);
+            // 
+            // allRad
+            // 
+            this.allRad.AutoSize = true;
+            this.allRad.Location = new System.Drawing.Point(497, 273);
+            this.allRad.Name = "allRad";
+            this.allRad.Size = new System.Drawing.Size(36, 17);
+            this.allRad.TabIndex = 24;
+            this.allRad.TabStop = true;
+            this.allRad.Text = "All";
+            this.allRad.UseVisualStyleBackColor = true;
+            this.allRad.CheckedChanged += new System.EventHandler(this.allRad_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(271, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 18);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Per cent";
+            // 
+            // textBoxPerCent
+            // 
+            this.textBoxPerCent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPerCent.Location = new System.Drawing.Point(367, 121);
+            this.textBoxPerCent.Name = "textBoxPerCent";
+            this.textBoxPerCent.Size = new System.Drawing.Size(124, 26);
+            this.textBoxPerCent.TabIndex = 25;
+            this.textBoxPerCent.Tag = "TextBoxPrice";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(271, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 18);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Markup price";
+            // 
+            // textBoxMarkupPrice
+            // 
+            this.textBoxMarkupPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxMarkupPrice.Location = new System.Drawing.Point(367, 35);
+            this.textBoxMarkupPrice.Name = "textBoxMarkupPrice";
+            this.textBoxMarkupPrice.Size = new System.Drawing.Size(124, 26);
+            this.textBoxMarkupPrice.TabIndex = 27;
+            this.textBoxMarkupPrice.Tag = "TextBoxMarkupPrice";
+            // 
+            // PerCentChange
+            // 
+            this.PerCentChange.Location = new System.Drawing.Point(383, 153);
+            this.PerCentChange.Name = "PerCentChange";
+            this.PerCentChange.Size = new System.Drawing.Size(75, 23);
+            this.PerCentChange.TabIndex = 29;
+            this.PerCentChange.Text = "Change";
+            this.PerCentChange.UseVisualStyleBackColor = true;
+            this.PerCentChange.Click += new System.EventHandler(this.PerCentChange_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1006, 568);
+            this.ClientSize = new System.Drawing.Size(1229, 634);
+            this.Controls.Add(this.PerCentChange);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxMarkupPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxPerCent);
+            this.Controls.Add(this.allRad);
+            this.Controls.Add(this.mouseRad);
+            this.Controls.Add(this.gameRad);
+            this.Controls.Add(this.keyRad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPrice);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxNumber);
             this.Controls.Add(this.BTN_CLEAT);
@@ -274,6 +412,17 @@
         private System.Windows.Forms.Button BTN_CLEAT;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.RadioButton keyRad;
+        private System.Windows.Forms.RadioButton gameRad;
+        private System.Windows.Forms.RadioButton mouseRad;
+        private System.Windows.Forms.RadioButton allRad;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxPerCent;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxMarkupPrice;
+        private System.Windows.Forms.Button PerCentChange;
     }
 }
 
--- Kursova_tovary/Kursova_tovary/Form1.cs
+++ Kursova_tovary/Kursova_tovary/Form1.cs
@@ -17,6 +17,8 @@ namespace Kursova_tovary
 {
     public partial class Form1 : Form
     {
+        static private string Category;
+
         public Form1()
         {
             InitializeComponent();
@@ -40,12 +42,19 @@ namespace Kursova_tovary
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MySqlCommand command = new MySqlCommand("SELECT PER_CENT FROM per_cent_index WHERE 1", DBUtils.GetDBConnection());
+
+            command.Connection.Open();
+            textBoxPerCent.Text = command.ExecuteScalar().ToString();
+            command.Connection.Close();
+
+            Category = "";
             FillDGV("");
         }
 
-        public void FillDGV(string valueToSearch)
-        {
-            MySqlCommand command = new MySqlCommand("SELECT * FROM product WHERE CONCAT (ID, NAME, DESCRIPTION, NUMBER) LIKE '%" + valueToSearch + "%'", DBUtils.GetDBConnection());
+        public void FillDGV(string valueToSearch)//Пошук значень через поле Search
+        {   
+            MySqlCommand command = new MySqlCommand("SELECT * FROM product WHERE CONCAT(ID, NAME, DESCRIPTION, NUMBER, PRICE) LIKE '%" + valueToSearch + "%' AND(ID) LIKE '" + Category + "%'", DBUtils.GetDBConnection());
             MySqlDataAdapter adapter = new MySqlDataAdapter(command);
             DataTable table = new DataTable();
             adapter.Fill(table);
@@ -53,38 +62,40 @@ namespace Kursova_tovary
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.DataSource = table;
             DataGridViewImageColumn imgcol = new DataGridViewImageColumn();
-            imgcol = (DataGridViewImageColumn)dataGridView1.Columns[4];
+            imgcol = (DataGridViewImageColumn)dataGridView1.Columns[5];
             imgcol.ImageLayout = DataGridViewImageCellLayout.Stretch;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
 
-        private void dataGridView1_Click(object sender, EventArgs e)
+        private void dataGridView1_Click(object sender, EventArgs e)//Автоматичне заповнення полів даними з таблиці
         {
-            Byte[] img = (Byte[])dataGridView1.CurrentRow.Cells[4].Value;
+            Byte[] img = (Byte[])dataGridView1.CurrentRow.Cells[5].Value;
             MemoryStream ms = new MemoryStream(img);
             pictureBox1.Image = Image.FromStream(ms);
             textBoxID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
             textBoxName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
             textBoxDesc.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
             textBoxNumber.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBoxPrice.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
         }
 
-        private void BTN_INSERT_Click(object sender, EventArgs e)
+        private void BTN_INSERT_Click(object sender, EventArgs e)//Додавання нових записів
         {
             MemoryStream ms = new MemoryStream();
             pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
             byte[] img = ms.ToArray();
 
-            MySqlCommand command = new MySqlCommand("INSERT INTO product(ID, NAME, DESCRIPTION, NUMBER, IMAGE) VALUES (@id, @name, @desc, @numb, @img)", DBUtils.GetDBConnection());
+            MySqlCommand command = new MySqlCommand("INSERT INTO product(ID, NAME, DESCRIPTION, NUMBER, PRICE, IMAGE) VALUES (@id, @name, @desc, @numb, @pr, @img)", DBUtils.GetDBConnection());
             command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxID.Text;
             command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBoxName.Text;
             command.Parameters.Add("@desc", MySqlDbType.VarChar).Value = textBoxDesc.Text;
-            command.Parameters.Add("@numb", MySqlDbType.VarChar).Value = textBoxDesc.Text;
+            command.Parameters.Add("@numb", MySqlDbType.VarChar).Value = textBoxNumber.Text;
+            command.Parameters.Add("@pr", MySqlDbType.VarChar).Value = textBoxPrice.Text;
             command.Parameters.Add("@img", MySqlDbType.Blob).Value = img;
             ExecMyQuery(command, "Data Inserted");
         }
         
-        public void ExecMyQuery(MySqlCommand mcomd, string myMsg)
+        public void ExecMyQuery(MySqlCommand mcomd, string myMsg)//Повідомлення про успішність вбо неуспішність введення даних
         {
             mcomd.Connection.Open();
             if(mcomd.ExecuteNonQuery() == 1)
@@ -99,22 +110,23 @@ namespace Kursova_tovary
             FillDGV(""); 
         }
 
-        private void BTN_UPDATE_Click(object sender, EventArgs e)
+        private void BTN_UPDATE_Click(object sender, EventArgs e)//Оноалення записів
         {
             MemoryStream ms = new MemoryStream();
             pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
             byte[] img = ms.ToArray();
 
-            MySqlCommand command = new MySqlCommand("UPDATE product SET NAME=@name ,DESCRIPTION=@desc, NUMBER = @numb, IMAGE=@img WHERE ID = @id", DBUtils.GetDBConnection());
+            MySqlCommand command = new MySqlCommand("UPDATE product SET NAME=@name ,DESCRIPTION=@desc, NUMBER = @numb, PRICE = @pr, IMAGE=@img WHERE ID = @id", DBUtils.GetDBConnection());
             command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBoxID.Text;
             command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBoxName.Text;
             command.Parameters.Add("@desc", MySqlDbType.VarChar).Value = textBoxDesc.Text;
             command.Parameters.Add("@numb", MySqlDbType.VarChar).Value = textBoxNumber.Text;
+            command.Parameters.Add("@pr", MySqlDbType.VarChar).Value = textBoxPrice.Text;
             command.Parameters.Add("@img", MySqlDbType.Blob).Value = img;
             ExecMyQuery(command, "Data Updated");
         }
 
-        private void BTN_DELETE_Click(object sender, EventArgs e)
+        private void BTN_DELETE_Click(object sender, EventArgs e)//Видалення записів
         {
             MySqlCommand command = new MySqlCommand("DELETE FROM product WHERE ID=@id", DBUtils.GetDBConnection());
 
@@ -128,7 +140,7 @@ namespace Kursova_tovary
             FillDGV(textBoxSearch.Text); 
         }
 
-        private void BTN_CLEAT_Click(object sender, EventArgs e)
+        private void BTN_CLEAT_Click(object sender, EventArgs e)//Очищення полів
         {
             ClearFields();
         }
@@ -139,7 +151,61 @@ namespace Kursova_tovary
             textBoxName.Text = "";
             textBoxDesc.Text = "";
             textBoxNumber.Text = "";
+            textBoxPrice.Text = "";
             pictureBox1.Image = null;
+        }
+
+        private void keyRad_CheckedChanged(object sender, EventArgs e)//Пошук в категорії клавіатур
+        {
+            if (keyRad.Checked)
+            {
+                textBoxSearch.Text = "";
+                Category = "K";
+                FillDGV("");
+            }
+        }
+
+        private void gameRad_CheckedChanged(object sender, EventArgs e)//Пошук в категорії геймпадів
+        {
+            if (gameRad.Checked)
+            {
+                textBoxSearch.Text = "";
+                Category = "G";
+                FillDGV("");
+            }
+        }
+
+        private void mouseRad_CheckedChanged(object sender, EventArgs e)//Пошук в категорії комп'ютерних мишок
+        {
+            if (mouseRad.Checked)
+            {
+                textBoxSearch.Text = "";
+                Category = "M";
+                FillDGV("");
+            }
+        }
+
+        private void allRad_CheckedChanged(object sender, EventArgs e)//Всі категорії
+        {
+            if (allRad.Checked)
+            {
+                textBoxSearch.Text = "";
+                Category = "";
+                FillDGV("");
+            }
+        }
+
+        private void PerCentChange_Click(object sender, EventArgs e)//Внесення значень проценту надбавки
+        {
+            MySqlCommand command = new MySqlCommand("UPDATE per_cent_index SET PER_CENT=@per_cent WHERE ID = 1", DBUtils.GetDBConnection());
+            command.Parameters.Add("@per_cent", MySqlDbType.Int32).Value = Int32.Parse(textBoxPerCent.Text);
+            ExecMyQuery(command, "Data Updated");
+        }
+
+        private void textBoxPrice_TextChanged(object sender, EventArgs e)//Вирахування ціни з урахуванням надбавки
+        {
+            double percent = Double.Parse(textBoxPrice.Text) + (Double.Parse(textBoxPrice.Text) * Double.Parse(textBoxPerCent.Text) / 100);
+            textBoxMarkupPrice.Text = percent.ToString();
         }
     }
 }
